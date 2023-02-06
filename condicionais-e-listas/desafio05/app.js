ef69991d7215fe8cef0c9cbeaa3390053bd35a08
Felipe Azevedo dos Santos <felipe.azevedo@cws.digital>
2023-02-06 16:20:58 -0300

    Listas desafio prático

--- condicionais-e-listas/desafio05/app.js
+++ condicionais-e-listas/desafio05/app.js
@@ -1,6 +1,7 @@
 new Vue({
 	el: '#desafio',
 	data: {
+		altenar: true,
 		array: ['Pedro', 'Bia', 'Ana', 'Rebeca'],
 		livro: {
 			titulo: 'O Senhor dos Anéis',
@@ -11,6 +12,11 @@ new Vue({
 			id: 10,
 			nome: 'Maria',
 			notas: [7.67, 8.33, 6.98, 9.21]
-		}
+		},
+		guitarras: [
+			{marca: 'ibanez', modelo: 'strato'},
+			{marca: 'gibson', modelo: 'les paul'},
+			{marca: 'fender', modelo: 'telecaster'}
+		]
 	}
 });
