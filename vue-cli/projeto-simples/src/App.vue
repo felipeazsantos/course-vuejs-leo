da67dbeeb3c2f48cbde232aad23b27823f5b0661
Felipe Azevedo dos Santos <felipe.azevedo@cws.digital>
2023-03-31 12:48:44 -0300

    vue-cli e componentes

---
+++ vue-cli/projeto-simples/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    {{ titulo }}
+    <button @click="titulo += '#'">Alterar</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: function() {
+      return {
+        titulo: 'Teste data usando VueJS'
+      }  
+    }
+  }
+</script>
+
+<style>
+
+  #app {
+    background-color: chocolate;
+    color: #fff;
+  }
+
+</style>
