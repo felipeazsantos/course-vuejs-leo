324a7c1e9268999880d13f254dfe8ce90f346886
Felipe Azevedo dos Santos <felipe.azevedo@cws.digital>
2023-04-06 15:29:54 -0300

    Animações no VueJS

---
+++ animações/animacoes-exercicios/src/AlertaAdvertencia.vue
@@ -0,0 +1,13 @@
+<template>
+    <b-alert variant="warning" show>mensagem alerta advertência!</b-alert>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
---
+++ animações/animacoes-exercicios/src/AlertaInfo.vue
@@ -0,0 +1,13 @@
+<template>
+    <b-alert variant="info" show>mensagem alerta info!</b-alert>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
