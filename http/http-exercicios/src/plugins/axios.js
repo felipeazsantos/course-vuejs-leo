edd4d5d0e51387ac90cb03301bd09317404c9c76
Felipe Azevedo dos Santos <felipe.azevedo@cws.digital>
2023-04-07 13:35:26 -0300

    Requisições http com Axios

---
+++ http/http-exercicios/src/plugins/axios.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import axios from 'axios'
+
+// axios.defaults.baseURL = 'https://curso-vue-leo-default-rtdb.firebaseio.com/'
+// axios.defaults.headers.common['Authorization'] = 'abc123'
+// axios.defaults.headers.get['Accepts'] = 'application/json'
+
+Vue.use({
+    install(Vue) {
+        Vue.prototype.$http = axios.create({
+            baseURL: 'https://curso-vue-leo-default-rtdb.firebaseio.com/',
+            headers: {
+                "Authorization": "abc123"
+            }
+        })
+
+        Vue.prototype.$http.interceptors.request.use(config => {
+            console.log(config.method)
+            return config
+        }, error => Promise.reject(error))
+
+        Vue.prototype.$http.interceptors.response.use(resp => {
+            // const array = []
+            // for (let chave in resp.data) {
+            //     array.push({id: chave, ...resp.data[chave]})
+            // }
+
+            // resp.data = array
+            return resp
+        }, error => Promise.reject(error))
+    }
+})
